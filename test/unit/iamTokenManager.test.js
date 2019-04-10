2f6a89f42a409566639b7414f7edc54eca57e69b
Joy Chang <joychang@us.ibm.com>
2019-04-10 14:10:31 -0500

    test: improve iamTokenManager test coverage

--- test/unit/iamTokenManager.test.js
+++ test/unit/iamTokenManager.test.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const requestWrapper = require('../../lib/requestwrapper');
+requestWrapper.sendRequest = jest.fn();
+
 const IamTokenManagerV1 = require('../../iam-token-manager/v1').IamTokenManagerV1;
 
 describe('iam_token_manager_v1', function() {
+  beforeEach(() => {
+    requestWrapper.sendRequest.mockReset();
+  });
+
+  afterAll(() => {
+    requestWrapper.sendRequest.mockRestore();
+  });
+
   it('should return an access token given by the user', function(done) {
     const userManagedToken = 'abcd-1234';
     const instance = new IamTokenManagerV1({ iamAccessToken: userManagedToken });
@@ -19,7 +30,6 @@ describe('iam_token_manager_v1', function() {
 
   it('should turn an iam apikey into an access token', function(done) {
     const instance = new IamTokenManagerV1({ iamApikey: 'abcd-1234' });
-    const requestMock = jest.spyOn(instance, 'requestToken');
     const refreshMock = jest.spyOn(instance, 'refreshToken');
 
     const accessToken = '9012';
@@ -30,13 +40,13 @@ describe('iam_token_manager_v1', function() {
       expires_in: 3600,
       expiration: Math.floor(Date.now() / 1000) + 3600,
     };
-    requestMock.mockImplementation(cb => {
-      cb(null, iamResponse);
+
+    requestWrapper.sendRequest.mockImplementation((parameters, _callback) => {
+      _callback(null, iamResponse);
     });
 
     instance.getToken(function(err, token) {
       expect(token).toBe(accessToken);
-      expect(requestMock).toHaveBeenCalled();
       expect(refreshMock).not.toHaveBeenCalled();
       done();
     });
@@ -45,7 +55,6 @@ describe('iam_token_manager_v1', function() {
   it('should refresh an expired access token', function(done) {
     const instance = new IamTokenManagerV1({ iamApikey: 'abcd-1234' });
     const requestMock = jest.spyOn(instance, 'requestToken');
-    const refreshMock = jest.spyOn(instance, 'refreshToken');
 
     const currentTokenInfo = {
       access_token: '1234',
@@ -66,13 +75,12 @@ describe('iam_token_manager_v1', function() {
       expiration: Math.floor(Date.now() / 1000) + 3600,
     };
 
-    refreshMock.mockImplementation(cb => {
-      cb(null, iamResponse);
+    requestWrapper.sendRequest.mockImplementation((parameters, _callback) => {
+      _callback(null, iamResponse);
     });
 
     instance.getToken(function(err, token) {
       expect(token).toBe(accessToken);
-      expect(refreshMock).toHaveBeenCalled();
       expect(requestMock).not.toHaveBeenCalled();
       done();
     });
@@ -123,6 +131,71 @@ describe('iam_token_manager_v1', function() {
       expect(token).toBe(accessToken);
       expect(refreshMock).not.toHaveBeenCalled();
       expect(requestMock).not.toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('should refresh an access token without expires_in field', function(done) {
+    const instance = new IamTokenManagerV1({ iamApikey: 'abcd-1234' });
+    const requestMock = jest.spyOn(instance, 'requestToken');
+
+    const currentTokenInfo = {
+      access_token: '1234',
+      refresh_token: '5678',
+      token_type: 'Bearer',
+      expiration: Math.floor(Date.now() / 1000),
+    };
+
+    instance.tokenInfo = currentTokenInfo;
+
+    const accessToken = '9012';
+    const iamResponse = {
+      access_token: accessToken,
+      refresh_token: '3456',
+      token_type: 'Bearer',
+      expires_in: 3600,
+      expiration: Math.floor(Date.now() / 1000) + 3600,
+    };
+
+    requestWrapper.sendRequest.mockImplementation((parameters, _callback) => {
+      _callback(null, iamResponse);
+    });
+
+    instance.getToken(function(err, token) {
+      expect(token).toBe(accessToken);
+      expect(requestMock).not.toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('should request a new token when refresh token does not have expiration field', function(done) {
+    const instance = new IamTokenManagerV1({ iamApikey: 'abcd-1234' });
+    const refreshMock = jest.spyOn(instance, 'refreshToken');
+
+    const currentTokenInfo = {
+      access_token: '1234',
+      refresh_token: '5678',
+      token_type: 'Bearer',
+    };
+
+    instance.tokenInfo = currentTokenInfo;
+
+    const accessToken = '9012';
+    const iamResponse = {
+      access_token: accessToken,
+      refresh_token: '3456',
+      token_type: 'Bearer',
+      expires_in: 3600,
+      expiration: Math.floor(Date.now() / 1000) + 3600,
+    };
+
+    requestWrapper.sendRequest.mockImplementation((parameters, _callback) => {
+      _callback(null, iamResponse);
+    });
+
+    instance.getToken(function(err, token) {
+      expect(token).toBe(accessToken);
+      expect(refreshMock).not.toHaveBeenCalled();
       done();
     });
   });
