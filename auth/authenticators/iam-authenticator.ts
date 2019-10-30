97a87e5e12d96f89d9629e27f70fd4dbd9db7495
Matthew Rattray <56044526+mgrattray@users.noreply.github.com>
2019-10-30 15:14:26 -0500

    Update auth/authenticators/iam-authenticator.ts
    Co-Authored-By: Dustin Popp <dpopp07@gmail.com>

--- auth/authenticators/iam-authenticator.ts
+++ auth/authenticators/iam-authenticator.ts
@@ -38,7 +38,7 @@ export interface Options extends BaseOptions {
 /**
  * The [[IamAuthenticator]] will use the user-supplied `apikey`
  * values to obtain a bearer token from a token server.  When the bearer token
- * expires, a new token is obtained from the token server. If specified the
+ * expires, a new token is obtained from the token server. If specified, the
  * optional mutually inclusive `clientId` and`clientSecret` pair can be used to
  * influence rate-limiting for requests to the IAM token server.
  *
