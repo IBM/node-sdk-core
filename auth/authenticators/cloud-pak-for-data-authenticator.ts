b9978ff96c8d5dc94064d1ef3e7ca7c2b62142ac
Matthew Rattray <56044526+mgrattray@users.noreply.github.com>
2019-10-30 16:09:21 -0500

    Update auth/authenticators/cloud-pak-for-data-authenticator.ts
    Co-Authored-By: Dustin Popp <dpopp07@gmail.com>

--- auth/authenticators/cloud-pak-for-data-authenticator.ts
+++ auth/authenticators/cloud-pak-for-data-authenticator.ts
@@ -30,7 +30,7 @@ export interface Options extends BaseOptions {
 }
 
 /**
- * The [[CloudPakForDataAuthenticator]] will use the user-supplied  url, username and password values to obtain
+ * The [[CloudPakForDataAuthenticator]] will use the user-supplied url, username and password values to obtain
  * a bearer token from a token server.  When the bearer token expires, a new token is obtained from the token server.
  *
  * The bearer token will be sent as an Authorization header in the form:
