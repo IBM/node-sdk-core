a9596c1aec279593b224adc2bd0c2f4aa386e867
Shuyu Chen <shuyuchen@ibm.com>
2020-01-09 10:49:53 -0600

    linted unit test file

--- test/unit/export-unit-test-utils.test.js
+++ test/unit/export-unit-test-utils.test.js
@@ -8,26 +8,26 @@ describe('Unit Test Utils', () => {
   });
 
   it('should have function checkUrlAndMethod', () => {
-		expect(unitTestUtils.checkUrlAndMethod).toBeDefined();
+    expect(unitTestUtils.checkUrlAndMethod).toBeDefined();
   });
 
-	it('should have function checkMediaHeaders', () => {
-		expect(unitTestUtils.checkMediaHeaders).toBeDefined();
+  it('should have function checkMediaHeaders', () => {
+    expect(unitTestUtils.checkMediaHeaders).toBeDefined();
   });
 
-	it('should have function checkUserHeader', () => {
-		expect(unitTestUtils.checkUserHeader).toBeDefined();
-	});
+  it('should have function checkUserHeader', () => {
+    expect(unitTestUtils.checkUserHeader).toBeDefined();
+  });
 
-	it('should have function checkForSuccessfulExecution', () => {
-		expect(unitTestUtils.checkForSuccessfulExecution).toBeDefined();
-	});
+  it('should have function checkForSuccessfulExecution', () => {
+    expect(unitTestUtils.checkForSuccessfulExecution).toBeDefined();
+  });
 
-	it('should have function getOptions', () => {
-		expect(unitTestUtils.getOptions).toBeDefined();
-	});
+  it('should have function getOptions', () => {
+    expect(unitTestUtils.getOptions).toBeDefined();
+  });
 
-	it('should have function expectToBePromise', () => {
-		expect(unitTestUtils.expectToBePromise).toBeDefined();
-	});
+  it('should have function expectToBePromise', () => {
+    expect(unitTestUtils.expectToBePromise).toBeDefined();
+  });
 });
