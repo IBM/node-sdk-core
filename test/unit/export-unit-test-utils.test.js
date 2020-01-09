a148da9ad882c97c1c4f1c5417c43958db584e3e
Shuyu Chen <10778821+SaltedCaramelCoffee@users.noreply.github.com>
2020-01-09 16:16:39 -0500

    fix: export unitTestUtils functions as a module (#76)
    * fix: export unitTestUtils as a module
    
    * added test to verify unitTestUtils and its functions are exported

---
+++ test/unit/export-unit-test-utils.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { unitTestUtils } = require('../../dist');
+
+describe('Unit Test Utils', () => {
+  it('should be defined', () => {
+    expect(unitTestUtils).toBeDefined();
+  });
+
+  it('should have function checkUrlAndMethod', () => {
+    expect(unitTestUtils.checkUrlAndMethod).toBeDefined();
+  });
+
+  it('should have function checkMediaHeaders', () => {
+    expect(unitTestUtils.checkMediaHeaders).toBeDefined();
+  });
+
+  it('should have function checkUserHeader', () => {
+    expect(unitTestUtils.checkUserHeader).toBeDefined();
+  });
+
+  it('should have function checkForSuccessfulExecution', () => {
+    expect(unitTestUtils.checkForSuccessfulExecution).toBeDefined();
+  });
+
+  it('should have function getOptions', () => {
+    expect(unitTestUtils.getOptions).toBeDefined();
+  });
+
+  it('should have function expectToBePromise', () => {
+    expect(unitTestUtils.expectToBePromise).toBeDefined();
+  });
+});
