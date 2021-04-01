006991bfe89c0671f9c053457cebe52cec924fed
Dustin Popp <dpopp07@gmail.com>
2021-04-01 15:53:43 -0500

    fix: stub sdk-test-helpers in browser environments (#132)
    The "sdk-test-helpers" file contains helper functions that generated SDKs use
    in their generated unit tests. These functions are never actually needed when
    running standard operational code, therefore they are never used in the browser.
    
    The `expect` library that we use for testing has a dependency that isn't browser
    friendly in certain scenarios. This causes the core and any SDKs importing it
    to throw errors in these scenarios (like the React framework).
    
    This change stubs the sdk-test-helpers in browser environments the same way we
    stub the credentials file reading code.

---
+++ lib/sdk-test-helpers.browser.ts
@@ -0,0 +1,24 @@
+// Dummy for browser
+export function checkUrlAndMethod(options, url: string, method: any) {
+  return;
+};
+
+export function checkMediaHeaders(createRequestMock, accept: string, contentType: string) {
+  return;
+};
+
+export function checkUserHeader(createRequestMock, userHeaderName: string, userHeaderValue: string) {
+  return;
+};
+
+export function checkForSuccessfulExecution(createRequestMock) {
+  return;
+};
+
+export function getOptions(createRequestMock) {
+  return;
+};
+
+export function expectToBePromise(sdkPromise) {
+  return;
+};
