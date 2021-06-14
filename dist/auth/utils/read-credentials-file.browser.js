8b37d502399f8126808fc8e22f978127a8c54572
Phil Adams <phil_adams@us.ibm.com>
2021-06-13 22:11:33 -0500

    Revert "Doc for main (abf604a10483194d0e46e3a1d2e4c27c20331867)"
    This reverts commit 2325480afaffb8464c147869e756157bb8c3511d.

--- dist/auth/utils/read-credentials-file.browser.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Dummy for browser
-function readCredentialsFile() {
-    return {};
-}
-exports.readCredentialsFile = readCredentialsFile;
--- dist/lib/content-type.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { FileObject } from "./helper";
-declare const _default: {
-    fromFilename: (file: String | File | Buffer | NodeJS.ReadableStream | FileObject) => string;
-    fromHeader: (buffer: Buffer) => string;
-};
-export default _default;
