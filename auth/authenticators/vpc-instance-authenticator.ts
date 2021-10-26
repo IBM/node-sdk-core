b032393468b526e5e34d76fd2a5dd1a76b13f022
Dustin Popp <dpopp07@gmail.com>
2021-10-26 10:35:36 -0500

    chore: review feedback

--- auth/authenticators/vpc-instance-authenticator.ts
+++ auth/authenticators/vpc-instance-authenticator.ts
@@ -87,7 +87,7 @@ export class VpcInstanceAuthenticator extends TokenRequestBasedAuthenticator {
 
   /**
    * Setter for the "profile_id" parameter to use when fetching the bearer token from the IAM token server.
-   * @param {string} scope A string that makes up the iamProfileId parameter
+   * @param {string} A string that makes up the iamProfileId parameter
    */
   public setIamProfileId(iamProfileId: string): void {
     this.iamProfileId = iamProfileId;
