fbaed724da84968f912c2b8848d2fa1e4563f8f1
Dustin Popp <dpopp07@gmail.com>
2021-10-26 10:32:41 -0500

    Update auth/authenticators/vpc-instance-authenticator.ts
    Co-authored-by: Norbert Biczo <pyrooka@users.noreply.github.com>

--- auth/authenticators/vpc-instance-authenticator.ts
+++ auth/authenticators/vpc-instance-authenticator.ts
@@ -76,7 +76,7 @@ export class VpcInstanceAuthenticator extends TokenRequestBasedAuthenticator {
 
   /**
    * Setter for the "profile_crn" parameter to use when fetching the bearer token from the IAM token server.
-   * @param {string} scope A string that makes up the iamProfileCrn parameter
+   * @param {string} A string that makes up the iamProfileCrn parameter
    */
   public setIamProfileCrn(iamProfileCrn: string): void {
     this.iamProfileCrn = iamProfileCrn;
