4de8a9d4fe35d595c1c1c65b4a2b033c6d5f8b42
Phil Adams <phil_adams@us.ibm.com>
2022-04-04 14:38:56 -0500

    fix: bump dependencies to avoid minimist vulnerability (#195)

--- scripts/typedoc/generate_typedoc.sh
+++
@@ -1,7 +0,0 @@
-./node_modules/.bin/typedoc --mode file --theme ./scripts/typedoc/theme --excludeExternals \
-    --out ./doc \
-    ./lib/base-service.ts ./lib/content-type.ts \
-    ./lib/helper.ts ./lib/querystring.ts \
-    ./lib/request-wrapper.ts \
-    ./lib/stream-to-promise.ts \
-    ./auth --target "ES5"
