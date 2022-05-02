5cb9081f581c73b3ba16e470eb4c03c9b8f5c266
Norbert Biczo <pyrooka@users.noreply.github.com>
2022-05-02 16:50:55 +0200

    fix: do not retry on 501 (#197)
    This commit changes the retry behavior a little,
    by disable retries on status code 501.

--- test/unit/retry.test.js
+++ test/unit/retry.test.js
@@ -90,6 +90,19 @@ describe('Node Core retries', () => {
     await service.createRequest(parameters).catch((err) => expect(err).toBeDefined());
   });
 
+  it('should not retry on 501`', async () => {
+    const scopes = [
+      nock(url).get('/').reply(501, undefined),
+      nock(url).get('/').reply(200, 'retry success!'),
+    ];
+
+    let err;
+    await service.createRequest(parameters).catch((error) => {
+      err = error;
+    });
+    expect(err).toBeDefined();
+  });
+
   it('should not retry after we call disableRetries', async () => {
     const scopes = [
       nock(url).get('/').reply(500, undefined),
